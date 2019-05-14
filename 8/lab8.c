824341e217b21cdcd96c748898eb2175efb870cc
Airat <45467135+airatb1508@users.noreply.github.com>
2019-05-15 01:32:14 +0300

    :q!

--- 8/lab8.c
+++ 8/lab8.c
@@ -13,27 +13,21 @@ int main() {
 	printf("Введите выражение:\n");
 	scanf("%s", start);
 
-	int pr[MAXDL], pos = 0, p = -1, tmp;
+	int pr[MAXDL], pos = 0, p = -1;
 	char op[MAXDL], el;
-	for(int i = 0; start[i] != '\0' && i < MAXDL; i++){
-		tmp = 0;
+	for(int i = 0; start[i] && start[i] != '\0' && i < MAXDL; i++){
 		el = start[i];
 		printf("%c\n", el);
 		if('a' <= el && el <= 'z'){
 			end[pos] = el;
 			pos++;
 		}
-		else if(el == '+' || el == '-' || el == '*' || el == '/' || el == ' '){
+		else if(el == '+' || el == '-' || el == '*' || el == '/'){
 			p++;
 			op[p] = el;
 			if(el == '+' || el == '-') pr[p] = 1;
-			else if (el == ' ') pr[p] = 0;
 			else pr[p] = 2;
-			printf("op:");
-			for(int j = 0; j <= p; j++) printf(" %c", op[j]);
-			printf("\npr:");
-			for(int j = 0; j <= p; j++) printf(" %d", pr[j]);
-			printf("\n");
+	
 		}
 		while(p > 0 && pr[p] <= pr[p-1]){
 			end[pos] = op[p-1];
@@ -41,14 +35,7 @@ int main() {
 			op[p-1] = op[p];
 			pr[p-1] = pr[p];
 			p--;
-			tmp++;
 		}
-		if (tmp != 0 && pr[p] == 0) break;
-		printf("op:");
-		for(int j = 0; j <= p; j++) printf(" %c", op[j]);
-		printf("\npr:");
-		for(int j = 0; j <= p; j++) printf(" %d", pr[j]);
-		printf("\n");
 	}
 	
 	printf("\nПоследовательность элементарных присваиваний:\n%s\n", end);
